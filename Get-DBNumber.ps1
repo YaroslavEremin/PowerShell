3eb3410b882fe80e3f3d0bff0000f173ac3ce8b5
Yaroslav <yaroslav@itwonline.ru>
2013-11-21 10:56:31 +0400

    replace -> match
    Signed-off-by: Yaroslav <yaroslav@itwonline.ru>

--- Get-DBNumber.ps1
+++ Get-DBNumber.ps1
@@ -11,7 +11,7 @@ $NumbersArray = @()
 [int]$Counter = 0
 $DBArray = (Get-MailboxDatabase | Where-Object -Property Name -like -Value "DB*" | Select-Object Name)
 ForEach ($DBName in $DBArray) {
-$Number = [regex]::replace([regex]::replace($DBName.Name,'-.*',''),'^DB','')
+$Number = [regex]::match($DBName.Name,'\d{2,3}').Value
 $NumbersArray += ($Number -as [int])
 }
 $NumbersArray = $NumbersArray | Sort-Object
@@ -22,5 +22,5 @@ $DBNumber = ($Counter + 1) -as [string]
 if ($DBNumber.Length -eq 1) {
 $DBNumber = "0$DBNumber"
 }
-Return $DBNumber
+Return "$DBNumber"
 }
