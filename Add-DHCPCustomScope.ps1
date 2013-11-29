df9a69e0b07765de3c068fb3a0e4c82647432bd5
Yaroslav <yaroslav@itwonline.ru>
2013-11-29 13:50:20 +0400

    new function Signed-off-by: Yaroslav <yaroslav@itwonline.ru>

--- Add-DHCPCustomScope.ps1
+++ Add-DHCPCustomScope.ps1
@@ -1,4 +1,197 @@
-﻿Function Get-NetworkSummary ( [String]$IP, [String]$Mask ) {
+﻿Function ConvertTo-BinaryIP {
+  <#
+    .Synopsis
+      Converts a Decimal IP address into a binary format.
+    .Description
+      ConvertTo-BinaryIP uses System.Convert to switch between decimal and binary format. The output from this function is dotted binary.
+    .Parameter IPAddress
+      An IP Address to convert.
+  #>
+ 
+  [CmdLetBinding()]
+  Param(
+    [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+    [Net.IPAddress]$IPAddress
+  )
+ 
+  Process {  
+    Return [String]::Join('.', $( $IPAddress.GetAddressBytes() |
+      ForEach-Object { [Convert]::ToString($_, 2).PadLeft(8, '0') } ))
+  }
+}
+
+Function ConvertTo-DecimalIP {
+  <#
+    .Synopsis
+      Converts a Decimal IP address into a 32-bit unsigned integer.
+    .Description
+      ConvertTo-DecimalIP takes a decimal IP, uses a shift-like operation on each octet and returns a single UInt32 value.
+    .Parameter IPAddress
+      An IP Address to convert.
+  #>
+   
+  [CmdLetBinding()]
+  Param(
+    [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+    [Net.IPAddress]$IPAddress
+  )
+ 
+  Process {
+    $i = 3; $DecimalIP = 0;
+    $IPAddress.GetAddressBytes() | ForEach-Object { $DecimalIP += $_ * [Math]::Pow(256, $i); $i-- }
+ 
+    Return [UInt32]$DecimalIP
+  }
+}
+
+Function ConvertTo-DottedDecimalIP {
+  <#
+    .Synopsis
+      Returns a dotted decimal IP address from either an unsigned 32-bit integer or a dotted binary string.
+    .Description
+      ConvertTo-DottedDecimalIP uses a regular expression match on the input string to convert to an IP address.
+    .Parameter IPAddress
+      A string representation of an IP address from either UInt32 or dotted binary.
+  #>
+ 
+  [CmdLetBinding()]
+  Param(
+    [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+    [String]$IPAddress
+  )
+   
+  Process {
+    Switch -RegEx ($IPAddress) {
+      "([01]{8}\.){3}[01]{8}" {
+        Return [String]::Join('.', $( $IPAddress.Split('.') | ForEach-Object { [Convert]::ToUInt32($_, 2) } ))
+      }
+      "\d" {
+        $IPAddress = [UInt32]$IPAddress
+        $DottedIP = $( For ($i = 3; $i -gt -1; $i--) {
+          $Remainder = $IPAddress % [Math]::Pow(256, $i)
+          ($IPAddress - $Remainder) / [Math]::Pow(256, $i)
+          $IPAddress = $Remainder
+         } )
+        
+        Return [String]::Join('.', $DottedIP)
+      }
+      default {
+        Write-Error "Cannot convert this format"
+      }
+    }
+  }
+}
+
+Function ConvertTo-MaskLength {
+  <#
+    .Synopsis
+      Returns the length of a subnet mask.
+    .Description
+      ConvertTo-MaskLength accepts any IPv4 address as input, however the output value 
+      only makes sense when using a subnet mask.
+    .Parameter SubnetMask
+      A subnet mask to convert into length
+  #>
+ 
+  [CmdLetBinding()]
+  Param(
+    [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+    [Alias("Mask")]
+    [Net.IPAddress]$SubnetMask
+  )
+ 
+  Process {
+    $Bits = "$( $SubnetMask.GetAddressBytes() | ForEach-Object { [Convert]::ToString($_, 2) } )" -Replace '[\s0]'
+ 
+    Return $Bits.Length
+  }
+}
+
+Function ConvertTo-Mask {
+  <#
+    .Synopsis
+      Returns a dotted decimal subnet mask from a mask length.
+    .Description
+      ConvertTo-Mask returns a subnet mask in dotted decimal format from an integer value ranging 
+      between 0 and 32. ConvertTo-Mask first creates a binary string from the length, converts 
+      that to an unsigned 32-bit integer then calls ConvertTo-DottedDecimalIP to complete the operation.
+    .Parameter MaskLength
+      The number of bits which must be masked.
+  #>
+   
+  [CmdLetBinding()]
+  Param(
+    [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+    [Alias("Length")]
+    [ValidateRange(0, 32)]
+    $MaskLength
+  )
+   
+  Process {
+    Return ConvertTo-DottedDecimalIP ([Convert]::ToUInt32($(("1" * $MaskLength).PadRight(32, "0")), 2))
+  }
+}
+
+Function Get-NetworkAddress {
+  <#
+    .Synopsis
+      Takes an IP address and subnet mask then calculates the network address for the range.
+    .Description
+      Get-NetworkAddress returns the network address for a subnet by performing a bitwise AND 
+      operation against the decimal forms of the IP address and subnet mask. Get-NetworkAddress
+      expects both the IP address and subnet mask in dotted decimal format.
+    .Parameter IPAddress
+      Any IP address within the network range.
+    .Parameter SubnetMask
+      The subnet mask for the network.
+  #>
+   
+  [CmdLetBinding()]
+  Param(
+    [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+    [Net.IPAddress]$IPAddress,
+     
+    [Parameter(Mandatory = $True, Position = 1)]
+    [Alias("Mask")]
+    [Net.IPAddress]$SubnetMask
+  )
+ 
+  Process {
+    Return ConvertTo-DottedDecimalIP ((ConvertTo-DecimalIP $IPAddress) -BAnd (ConvertTo-DecimalIP $SubnetMask))
+  }
+}
+
+Function Get-BroadcastAddress {
+  <#
+    .Synopsis
+      Takes an IP address and subnet mask then calculates the broadcast address for the range.
+    .Description
+      Get-BroadcastAddress returns the broadcast address for a subnet by performing a bitwise AND 
+      operation against the decimal forms of the IP address and inverted subnet mask. 
+      Get-BroadcastAddress expects both the IP address and subnet mask in dotted decimal format.
+    .Parameter IPAddress
+      Any IP address within the network range.
+    .Parameter SubnetMask
+      The subnet mask for the network.
+  #>
+   
+  [CmdLetBinding()]
+  Param(
+    [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
+    [Net.IPAddress]$IPAddress, 
+     
+    [Parameter(Mandatory = $True, Position = 1)]
+    [Alias("Mask")]
+    [Net.IPAddress]$SubnetMask
+  )
+ 
+  Process {
+    Return ConvertTo-DottedDecimalIP $((ConvertTo-DecimalIP $IPAddress) -BOr `
+      ((-BNot (ConvertTo-DecimalIP $SubnetMask)) -BAnd [UInt32]::MaxValue))
+  }
+}
+
+Function Get-NetworkSummary ( [String]$IP, [String]$Mask ) {
   If ($IP.Contains("/"))
   {
     $Temp = $IP.Split("/")
@@ -53,6 +246,7 @@
    
   Return $NetInfo
 }
+
 Function Get-NetworkRange( [String]$IP, [String]$Mask ) {
   If ($IP.Contains("/"))
   {
@@ -83,61 +277,77 @@ Function Add-DHCPCustomScope {
       ???
     .Description
       ?
-    .Parameter IPAddress
-      ?
-    .Parameter SubnetMask
-      ?
+    .EXAMPLE
+    Add-DHCPCustomScope -i "10.123.151.0" -su "24" -sc "I" -dh "dc-07" -Add "info-terminal" -Venue "SLV"
+    .EXAMPLE
   #>
    
   [CmdLetBinding()]
   Param(
     [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $True)]
-    [Net.IPAddress]$IPAddress, 
+    [String]$IPAddress, 
      
     [Parameter(Mandatory = $True, Position = 1, ValueFromPipeline = $True)]
-    [Net.IPAddress]$SubnetMask,
+    [String]$SubnetMask,
 
     [Parameter(Mandatory = $True, Position = 2, ValueFromPipeline = $True)]
-    [Net.IPAddress]$ScopeType,
+    [String]$ScopeType,
 
     [Parameter(Mandatory = $True, Position = 3, ValueFromPipeline = $True)]
-    [Net.IPAddress]$Venue
+    [String]$DHCP,
+
+    [Parameter(Mandatory = $True, Position = 3, ValueFromPipeline = $True)]
+    [String]$Venue,
+
+    [Parameter(Mandatory = $False, Position = 3, ValueFromPipeline = $True)]
+    [String]$Add
   )
 
 $ScopeInfo = Get-NetworkSummary $IPAddress $SubnetMask
-$ScopeName = "AVAYA_" + $NetArea + "_" + $NetType + "_" + $Venue
+$ScopeInfo
 $FirstIPAddress = [regex]::Replace($ScopeInfo.Range,"\s.*","0")
+$FirstIPAddress
 $Gateway = [regex]::Replace($ScopeInfo.Range,"\s.*","")
+$Gateway
 $LastIPAddress = [regex]::Replace($ScopeInfo.Range,"^\S*\s-\s","")
-
-&netsh dhcp server $DHCP add scope $IPAddress $ScopeInfo.Mask Temp Temp
-&netsh dhcp server $DHCP scope $IPAddress add iprange $FirstIPAddress $LastIPAddress
-&netsh dhcp server $DHCP scope $IPAddress set optionvalue 003 IPADDRESS $Gateway
+$LastIPAddress
+$DHCP = "\\" + $DHCP
+&netsh dhcp server $DHCP add scope $IPAddress $ScopeInfo.Mask Temp Temp | Out-Null
+&netsh dhcp server $DHCP scope $IPAddress add iprange $FirstIPAddress $LastIPAddress | Out-Null
+&netsh dhcp server $DHCP scope $IPAddress set optionvalue 003 IPADDRESS $Gateway | Out-Null
 
  Switch -regex ($ScopeType) {
- {A} {
+ "A" {
     $NetArea = "Admin"
-    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 252 STRING  "----"
+    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 252 STRING  "http://172.16.22.150/wpad.dat" | Out-Null
     }
- {G} {
+ "I" {
+    $NetArea = "Games"
+    $NetType = $Add
+    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 242 STRING  "L2Q=1,L2QVLAN=48" | Out-Null
+    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 252 STRING  "http://172.16.22.150/wpad.dat" | Out-Null
+    }
+ "G" {
     $NetArea = "Guest"
-    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 006 IPADDRESS 8.8.8.8 8.8.4.4
+    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 006 IPADDRESS 8.8.8.8 8.8.4.4 | Out-Null
     }
- {M} {
+ "M" {
     $NetArea = "Mng"
     }
- {V} {
+ "V" {
     $NetType = "Voice"
-    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 242 STRING  "----"
+    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 242 STRING  "L2Q=1,L2QVLAN=48,MCIPADD=10.29.33.60,10.3.0.2,HTTPSRVR=172.16.22.31,HTTPDIR= /http96xx/" | Out-Null
     }
- {W} {
+ "W" {
     $NetType = "WiFi"
-    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 043 BYTE  "----"
+    &netsh dhcp server $DHCP scope $IPAddress set optionvalue 043 BYTE  "" | Out-Null
     }
  }
-
- &netsh dhcp server $DHCP scope $IPAddress set state 1
- &netsh dhcp server $DHCP scope $IPAddress set comment $ScopeName
- &netsh dhcp server $DHCP scope $IPAddress set name $ScopeName
-
+ $NetArea
+ $NetType
+ $ScopeName = "AVAYA_" + $NetArea + "_" + $NetType + "_" + $Venue
+ $ScopeName
+ &netsh dhcp server $DHCP scope $IPAddress set comment $ScopeName | Out-Null
+ &netsh dhcp server $DHCP scope $IPAddress set name $ScopeName | Out-Null
+ &netsh dhcp server $DHCP scope $IPAddress set state 1 | Out-Null
 }
---
+++ Move-ADUsersToFilial.ps1
@@ -0,0 +1,62 @@
+﻿Function Move-ADUsersToFilial {
+<#
+  .SYNOPSIS
+  ????
+  .DESCRIPTION
+  ????
+  .EXAMPLE
+  ????
+  #>
+$LogPath="D:\logs\" + (Get-Date).tostring("yyyyMMdd") + ".txt"
+If( Test-Path $BackupFolder ) {
+write-host -ForegroundColor Green "Logfile --> $LogPath"
+$Bases = (Get-MailboxDatabase) | Where-Object {$_.Server -like 'EXCH-MB2*' -and -not($_.Name -like "EXT-*")  } | Select-Object Name,@{Name='Type';Expression={[regex]::replace($_.name,'^.*-','')}},@{Name="Mailboxes";Expression={(@(Get-Mailbox -Database $_.name)).Count}}
+$Bases200M = $Bases | Where-Object {$_.Type -eq '200M'} | Sort-Object Mailboxes
+$Bases1G = $Bases | Where-Object {$_.Type -eq '1G'} | Sort-Object Mailboxes
+$Bases2G = $Bases | Where-Object {$_.Type -eq '2G'} | Sort-Object Mailboxes
+$Bases5G = $Bases | Where-Object {$_.Type -eq '5G'} | Sort-Object Mailboxes
+$Bases20G = $Bases | Where-Object {$_.Type -eq '20G'} | Sort-Object Mailboxes
+$DB200M = $Bases200M[0].Name
+write-host -ForegroundColor Green "$DB200M"
+$DB200M | Out-File -Encoding unicode -FilePath $LogPath
+$DB1G = $Bases1G[0].Name
+write-host -ForegroundColor Green "$DB1G"
+$DB1G | Out-File -Encoding unicode -FilePath $LogPath -Append
+$DB2G = $Bases2G[0].Name
+write-host -ForegroundColor Green "$DB2G"
+$DB2G | Out-File -Encoding unicode -FilePath $LogPath -Append
+$DB5G = $Bases5G[0].Name
+write-host -ForegroundColor Green "$DB5G"
+$DB5G | Out-File -Encoding unicode -FilePath $LogPath -Append
+$DB20G = $Bases20G[0].Name
+write-host -ForegroundColor Green "$DB20G"
+$DB20G | Out-File -Encoding unicode -FilePath $LogPath -Append
+$MovingUsers = Get-ADUser -SearchBase "OU=Moscow,DC=SOCHI-2014,DC=RU"-Filter {extFilial -like '*'} -Properties description,extFilial,distinguishedName | Select-Object -Property sAMAccountName,description,extFilial,distinguishedName
+#$MovingUsers = Get-ADUser -Identity mfedosov -Properties description,extFilial,distinguishedName | Select-Object -Property sAMAccountName,description,extFilial,distinguishedName
+$MovingUsers | Out-File -Encoding unicode -FilePath $LogPath -Append
+$MovingUsers.sAMAccountName
+Get-MoveRequest -MoveStatus Completed | Remove-MoveRequest
+ForEach ( $User in $MovingUsers )
+{
+    $NewPath = [regex]::match($User.distinguishedName,'OU.*').Value
+    $NewPath = [regex]::replace($NewPath,'Moscow','Sochi')
+    ($User.sAMAccountName + "-->" + $NewPath)
+    Out-File -InputObject ($User.sAMAccountName + "-->" + $NewPath) -Encoding unicode -FilePath $LogPath -Append
+    #Get-ADUser -Identity $User.sAMAccountName | Move-ADObject -TargetPath $NewPath
+    $MailboxDB = (Get-Mailbox -Identity $User.sAMAccountName).database
+    Switch -RegEx ($MailboxDB)
+    {
+        "200M" {New-MoveRequest -Identity $User.sAMAccountName -TargetDatabase $DB200M}
+        "1G" {New-MoveRequest -Identity $User.sAMAccountName -TargetDatabase $DB1G}
+        "2G" {New-MoveRequest -Identity $User.sAMAccountName -TargetDatabase $DB2G}
+        "5G" {New-MoveRequest -Identity $User.sAMAccountName -TargetDatabase $DB5G}
+        "20G" {New-MoveRequest -Identity $User.sAMAccountName -TargetDatabase $DB20G}
+    } 
+}
+$MoveRequests = Get-MoveRequest | Select-Object -Property Alias,Status,@{Name='SourceDB';Expression={$_.SourceDatabase}},@{Name="TargetDB";Expression={$_.TargetDatabase}} | Sort-Object Alias | Format-Table -AutoSize
+$MoveRequests
+$MoveRequests | Out-File -Encoding unicode -FilePath $LogPath -Append
+} else {
+}
+write-host -ForegroundColor Red "Folder for logs does not exist"
+}
